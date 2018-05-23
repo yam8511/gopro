e7141b9b1adff504bb02ca02bb4d644edff0f5d9
Zuolar <yam8511@gmail.com>
2018-05-24 01:06:18 +0800

    md5 and time format

--- hello.go
+++ hello.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
+	"log"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	n := time.Now()
+	t := n.Format("2006-01-02T15:04:05Z07:00")
+	t2 := n.Format(time.RFC3339)
+	// hash := []byte{}
+	hash := fmt.Sprintf("%x", md5.Sum([]byte(t)))
+	hash2 := fmt.Sprintf("%x", md5.Sum([]byte(t2)))
+	log.Printf("Time -> %s , Hash -> %s", t, hash)
+	log.Printf("Time -> %s , Hash -> %s", t2, hash2)
 }
