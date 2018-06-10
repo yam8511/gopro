eb75642807f998d8502f312b416c7301579e8784
Zuolar <yam8511@gmail.com>
2018-06-10 11:57:02 +0800

    學習鮑魚範例

--- hello.go
+++ hello.go
@@ -11,36 +11,30 @@ import (
 
 func main() {
 	// 讀取資料
-	rawData, err := base.ParseCSVToInstances("tall.csv", true)
+	rawData, err := base.ParseCSVToInstances("dataset/abalone/data.csv", true)
 	if err != nil {
 		panic(err)
 	}
 
 	// 將資料切分成訓練資料與測試資料
-	// trainData, testData := base.InstancesTrainTestSplit(rawData, 0.5)
+	trainData, testData := base.InstancesTrainTestSplit(rawData, 0.1)
 
 	// 建立一個訓練用的工具，cls即是Model
 	cls := knn.NewKnnClassifier("euclidean", "linear", 1)
 
 	// 開始訓練
-	err = cls.Fit(rawData)
+	err = cls.Fit(trainData)
 	if err != nil {
 		panic(err)
 	}
 
 	// 儲存Model
-	cls.Save("tall.model")
+	cls.Save("abalone.model")
 
-	// 取未知資料
-	unknown, err := base.ParseCSVToInstances("./unknown.csv", true)
-	if err != nil {
-		panic(err)
-	}
-	log.Println("未知資料 -> ", unknown)
-	// log.Println("測試資料 -> ", testData)
+	log.Println("測試資料 -> ", testData)
 
 	// 預測測試資料
-	prediction, err := cls.Predict(unknown)
+	prediction, err := cls.Predict(testData)
 	if err != nil {
 		panic(err)
 	}
@@ -48,7 +42,7 @@ func main() {
 	log.Println("預測結果 -> ", prediction)
 
 	// 將測試資料與預測結果，轉化為混淆矩陣
-	confusionMat, err := evaluation.GetConfusionMatrix(unknown, prediction)
+	confusionMat, err := evaluation.GetConfusionMatrix(testData, prediction)
 	if err != nil {
 		panic(fmt.Sprintf("Unable to get confusion matrix: %s", err.Error()))
 	}
