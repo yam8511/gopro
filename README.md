ec2463fa1cdb2741a55a1d38fa290d3988a1d662
Zuolar <yam8511@gmail.com>
2018-06-30 13:16:16 +0800

    顯示文字，偵測鍵盤

---
+++ README.md
@@ -0,0 +1,12 @@
+# Installation
+```
+go get github.com/gopherjs/gopherjs
+go get github.com/gopherjs/gopherwasm/js
+go get github.com/hajimehoshi/ebiten
+```
+
+# Excute
+```
+gopherjs serve --http :8888
+```
+see [browser](http://127.0.0.1:8888/gopro)
--- hello.go
+++ hello.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
+func update(screen *ebiten.Image) error {
+	ebitenutil.DebugPrint(screen, "Hello world!")
+	// 當「按鍵上」被按下時⋯⋯
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+		ebitenutil.DebugPrint(screen, "You're pressing the 'UP' button.")
+	}
+	// 當「按鍵下」被按下時⋯⋯
+	if ebiten.IsKeyPressed(ebiten.KeyDown) {
+		ebitenutil.DebugPrint(screen, "\nYou're pressing the 'DOWN' button.")
+	}
+	// 當「按鍵左」被按下時⋯⋯
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		ebitenutil.DebugPrint(screen, "\n\nYou're pressing the 'LEFT' button.")
+	}
+	// 當「按鍵右」被按下時⋯⋯
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		ebitenutil.DebugPrint(screen, "\n\n\nYou're pressing the 'RIGHT' button.")
+	}
+	return nil
+}
+
 func main() {
-	fmt.Println("Hello World")
+	ebiten.Run(update, 320, 240, 2, "Hello world!")
+	fmt.Println("OKOKO")
 }
