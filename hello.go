026e6080bfb96613a7eda2110e7a18f963af24bd
Zuolar <yam8511@gmail.com>
2018-07-05 00:57:49 +0800

    計時器

--- hello.go
+++ hello.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	second := flag.Int("s", 1, "Ticker Second")
+	flag.Parse()
+	ticker := time.NewTicker(time.Second * time.Duration(*second))
+	log.Println("Di Da")
+	for range ticker.C {
+		log.Println("Di Da")
+	}
 }
