9d34c896b43de55993fb8e4ee2782b2d30359d62
Zuolar <yam8511@gmail.com>
2018-07-05 02:01:44 +0800

    用看看 cronexpr，來取得 cron express的上一次的執行時間

--- hello.go
+++ hello.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"time"
+
+	"github.com/gorhill/cronexpr"
 )
 
 func main() {
 	fmt.Println("Hello World")
+	now := time.Now()
+	parsedTime, parseErr := cronexpr.Parse("*/5 * * * * * *")
+	if parseErr != nil {
+		log.Fatal("Parse Error ->", parseErr)
+		return
+	}
+	nextTime := parsedTime.NextN(now, 2)
+	lastTime := time.Unix(nextTime[0].Unix()-(nextTime[1].Unix()-nextTime[0].Unix()), 0)
+	fmt.Println("Now ->", now)
+	fmt.Println("Last ->", lastTime)
+	fmt.Println(nextTime)
 }
