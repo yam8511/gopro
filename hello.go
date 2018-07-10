562540bfd6c2a6fc7c32ee97e4198d13bae6c83b
Zoular <yam8511_li@mail.chungyo.net>
2018-07-10 15:15:39 +0800

    http範例

--- hello.go
+++ hello.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Hello World")
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello"))
+	})
+
+	err := http.ListenAndServe(":8000", mux)
+	if err != nil {
+		return
+	}
 }
