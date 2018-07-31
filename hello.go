3a6dded10ed7e8b5137d29214445d5b8b39a802d
Zuolar <yam8511@gmail.com>
2018-08-01 03:10:04 +0800

    加上cookie與敏感資料加密解密的方式

--- hello.go
+++ hello.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +12,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var commonIV = []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
+
+//aes的加密字符串
+var aesKey = "astaxie12798akljzmknm.ahkjkljlaa"
+
 func main() {
 	fmt.Println("Hello World")
 
@@ -16,15 +24,44 @@ func main() {
 
 	api := r.Group("/api")
 	{
-		demo := api.Group("demo")
-		demo.GET("id", func(c *gin.Context) {
-			fmt.Println("Keys ---> ", c.Keys)
+		api.GET("/login", func(c *gin.Context) {
+			session, err := EncryptSecureData([]byte("I'm Zuolar"))
+			if err != nil {
+				c.JSON(http.StatusOK, "Login Failed ---> "+err.Error())
+				return
+			}
+			cookie := &http.Cookie{
+				Name:  "session",
+				Value: session,
+			}
+			http.SetCookie(c.Writer, cookie)
+			c.JSON(http.StatusOK, "Login Done")
+		})
+
+		api.GET("user", func(c *gin.Context) {
+			session, err := c.Cookie("session")
+			if err != nil {
+				c.AbortWithStatusJSON(http.StatusOK, "login first")
+				return
+			}
+
+			data, err := DecyptSecureData(session)
+			if err != nil {
+				c.AbortWithStatusJSON(http.StatusOK, "login first")
+				return
+			}
+
+			if string(data) != "I'm Zuolar" {
+				c.AbortWithStatusJSON(http.StatusOK, "login first")
+				return
+			}
+
 			c.Set("name", time.Now().Unix())
 			a := c.Query("a")
 			fmt.Println("Get Query a --->", a)
 			c.Next()
 			cd := c.Query("c")
-			fmt.Println("Get Query cd --->", cd)
+			fmt.Println("Get Query c --->", cd)
 		}, func(c *gin.Context) {
 			name := c.GetInt64("name")
 			b := c.Query("b")
@@ -36,4 +73,37 @@ func main() {
 
 	err := r.Run(":8000")
 	log.Fatal(err)
+}
+
+// EncryptSecureData 加密敏感資料
+func EncryptSecureData(data []byte) (string, error) {
+	// 创建加密算法aes
+	c, err := aes.NewCipher([]byte(aesKey))
+	if err != nil {
+		return "", err
+	}
+
+	//加密字符串
+	cfb := cipher.NewCFBEncrypter(c, commonIV)
+	session := make([]byte, len(data))
+	cfb.XORKeyStream(session, data)
+	fmt.Printf("%s=>%x\n", string(data), session)
+	return fmt.Sprintf("%x", session), nil
+}
+
+// DecyptSecureData 解密敏感資料
+func DecyptSecureData(session string) ([]byte, error) {
+	s, err := hex.DecodeString(session)
+	// 创建加密算法aes
+	c, err := aes.NewCipher([]byte(aesKey))
+	if err != nil {
+		return nil, err
+	}
+
+	// 解密字符串
+	cfbdec := cipher.NewCFBDecrypter(c, commonIV)
+	data := make([]byte, len(s))
+	cfbdec.XORKeyStream(data, s)
+	fmt.Printf("%x=>%s\n", s, data)
+	return data, nil
 }
