93dcdd4d155e7b4a2cd9d2d50fab5e55ef9dd72b
Zoular <yam8511_li@mail.chungyo.net>
2018-08-04 17:20:08 +0800

    interface 的說明

--- hello.go
+++ hello.go
@@ -1,9 +1,45 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type demo struct {
+}
+
+func (demo) GetMe() {
+}
+
+type demo2 struct {
+}
+
+func (*demo2) GetMe() {
+}
 
 func main() {
-	fmt.Println("Hello World")
+	// 1. 定義規則
+	// 2. 實現規則
+	// 3. 執行結果
+
+	// 可為任意值
+	var data interface {
+		GetMe()
+	}
+	fmt.Println("data --->", data)
+
+	// 有實現 GetMe 的 func
+	newDemo := demo{}
+	data = newDemo
+	fmt.Println("data --->", data)
+
+	// 會噴錯，實現 GetMe 的 func 不能有 pointer
+	newDemo2 := demo2{}
+	newDemo2.GetMe()
+	data = newDemo2
+
+	// 會噴錯，int 並沒有實現 GetMe
+	data = 1
+	fmt.Println("data --->", data)
+
+	// 會噴錯，string 並沒有實現 GetMe
+	data = "OK"
+	fmt.Println("data --->", data)
 }
