3547d0feec2a760875a5846f318ba721adb9b7cc
Zuolar <yam8511@gmail.com>
2018-08-10 01:12:12 +0800

    加入圖檔

--- hello.go
+++ hello.go
@@ -9,10 +9,25 @@ import (
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
+var bg *ebiten.Image
+
 func update(screen *ebiten.Image) error {
 	// 先畫圖
-	c := color.NRGBA{0xff, 0x00, 0x00, 0xff}
+	c := color.NRGBA{0x33, 0x33, 0x33, 0xFF}
 	screen.Fill(c)
+
+	opts := &ebiten.DrawImageOptions{}
+	// opts.SourceRect.Max = image.Point{320, 240}
+	// opts.SourceRect.Min = image.Point{0, 0}
+	// opts.GeoM.Translate(float64(x), float64(y))
+	w, h := bg.Size()
+	opts.GeoM.Scale(
+		320/float64(w),
+		320/float64(h),
+	)
+
+	// 渲染 square 畫布到 screen 主畫布上並套用空白選項
+	screen.DrawImage(bg, opts)
 
 	// 在印字
 	FPS := fmt.Sprintf("\n\n\n\n\nFPS: %f", ebiten.CurrentFPS())
@@ -23,17 +38,6 @@ func update(screen *ebiten.Image) error {
 
 	// 顯示一段「X: xx, Y: xx」格式文字
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("X: %d, Y: %d", x, y))
-
-	square, err := ebiten.NewImage(16, 16, ebiten.FilterNearest)
-	if err != nil {
-		return err
-	}
-	square.Fill(color.White)
-
-	// opts.GeoM.Rotate(32)
-
-	// 渲染 square 畫布到 screen 主畫布上並套用空白選項
-	// screen.DrawImage(square, opts)
 
 	// 當「按鍵上」被按下時⋯⋯
 	if ebiten.IsKeyPressed(ebiten.KeyUp) {
@@ -58,6 +62,12 @@ func update(screen *ebiten.Image) error {
 
 	// 當「滑鼠左鍵」被按下時⋯⋯
 	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		square, err := ebiten.NewImage(16, 16, ebiten.FilterNearest)
+		if err != nil {
+			return err
+		}
+		square.Fill(color.White)
+
 		// 從 CursorPosition() 取得 X, Y 座標
 		x, y = ebiten.CursorPosition()
 		// 建立一個空白選項建構體
@@ -80,6 +90,13 @@ func update(screen *ebiten.Image) error {
 }
 
 func main() {
-	err := ebiten.Run(update, 320, 240, 2, "Hello world!")
+	var err error
+	bg, err = ebitenutil.NewImageFromURL("https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Go_Logo_Aqua.svg/1200px-Go_Logo_Aqua.svg.png")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = ebiten.Run(update, 320, 240, 2, "Hello world!")
 	fmt.Println(err)
 }
