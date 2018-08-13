6ba2da3db8c0e554019edaa74a0ccd4d7c563c59
Zoular <yam8511_li@mail.chungyo.net>
2018-08-13 15:13:36 +0800

    比較一般的字串相加與strings.Builder的差別

--- hello.go
+++ hello.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello World")
+
+	// 一般字串相加
+	begin := time.Now()
+	ss := "Hi there "
+	for i := 0; i < 100000; i++ {
+		ss += "(" + fmt.Sprint(i) + "," + fmt.Sprint(i) + "),"
+	}
+	fmt.Println("一般字串相加 ---> ", time.Since(begin))
+
+	// 使用 string.Builder 做字串相加
+	begin = time.Now()
+	s := new(strings.Builder)
+	s.WriteString("Hi there ")
+	for i := 0; i < 100000; i++ {
+		s.WriteString("(" + fmt.Sprint(i) + "," + fmt.Sprint(i) + "),")
+	}
+	fmt.Println("strings.Builder ---> ", time.Since(begin))
+
+	// 驗證字串是否相同
+	same := s.String() == ss
+	fmt.Println("兩者組成的字串是否相同？", same)
 }
