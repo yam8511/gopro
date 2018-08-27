0dd8fbd5d9e5f18b1348f7450b715a954f1cee32
Zoular <yam8511_li@mail.chungyo.net>
2018-08-27 12:27:21 +0800

    隨機產生n個字元的字串

--- hello.go
+++ hello.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
+)
+
+const (
+	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
 )
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello World ---> ", StrRandom(3))
+}
+
+// StrRandom 亂數產生字串
+func StrRandom(n int) string {
+	var src = rand.NewSource(time.Now().UnixNano())
+
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
 }
