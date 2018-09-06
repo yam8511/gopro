2a0164d3ec324355d5a85f89d3637f39ea5070bf
Zuolar <yam8511@gmail.com>
2018-09-07 00:42:15 +0800

    簡單reflect取標籤的範例

--- hello.go
+++ hello.go
@@ -9,9 +9,9 @@ import (
 
 // Photo 照片，以下的「photo」是我自取的標籤名稱
 type Photo struct {
-	Name      string    `photo:"name"`
-	Size      int       `photo:"size"`
-	CreatedAt time.Time `photo:"created_at"`
+	Name      string    `photo:"name" pic:"pic_name"` // 有給 photo & pic 的標籤
+	Size      int       `photo:"size" pic:"pic_size"` // 有給 photo & pic 的標籤
+	CreatedAt time.Time `photo:"created_at"`          // 只有給 photo 的標籤
 }
 
 func main() {
@@ -24,7 +24,8 @@ func main() {
 	// 顯示資料屬性
 	fmt.Printf("Photo 這個物件有幾個欄位？ %d 個\n", handsomeType.NumField())
 	fieldName := []string{}
-	tagName := []string{}
+	tagPhotoName := []string{}
+	tagPicName := []string{}
 	for i := 0; i < handsomeType.NumField(); i++ {
 		// 欄位資料
 		fieldInfo := handsomeType.Field(i)
@@ -32,13 +33,27 @@ func main() {
 
 		// 標籤資料
 		tagInfo := fieldInfo.Tag
+
 		// 尋找是否有「photo」的標籤
-		valueInTag, hasTag := tagInfo.Lookup("photo")
+		valueInTag, hasPhotoTag := tagInfo.Lookup("photo")
 		// 如果有「photo」的標籤資料，存進去變數
-		if hasTag {
-			tagName = append(tagName, valueInTag)
+		if hasPhotoTag {
+			tagPhotoName = append(tagPhotoName, valueInTag)
+		} else {
+			tagPhotoName = append(tagPhotoName, "<沒資料>")
 		}
+
+		// 尋找是否有「pic」的標籤
+		valueInTag, hasPicTag := tagInfo.Lookup("pic")
+		// 如果有「pic」的標籤資料，存進去變數
+		if hasPicTag {
+			tagPicName = append(tagPicName, valueInTag)
+		} else {
+			tagPicName = append(tagPicName, "<沒資料>")
+		}
+
 	}
 	fmt.Printf("欄位名稱分別是 %s \n", strings.Join(fieldName, ", "))
-	fmt.Printf("標籤「photo」的資料分別是 %s \n", strings.Join(tagName, ", "))
+	fmt.Printf("標籤「photo」的資料分別是 %s \n", strings.Join(tagPhotoName, ", "))
+	fmt.Printf("標籤「pic」的資料分別是 %s \n", strings.Join(tagPicName, ", "))
 }
