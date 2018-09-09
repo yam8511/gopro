cec9d9776d4ba9f71c810197ba61ed9a35cec20b
Zuolar <yam8511@gmail.com>
2018-09-10 01:22:16 +0800

    QRCode練習

--- hello.go
+++ hello.go
@@ -2,8 +2,46 @@ package main
 
 import (
 	"fmt"
+	"image/color"
+	"os"
+
+	"github.com/skip2/go-qrcode"
+	qrdecode "github.com/tuotoo/qrcode"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	// err := qrcode.WriteFile(
+	// 	"https://tofu-rosa.herokuapp.com/graphql",
+	// 	qrcode.Medium,
+	// 	256,
+	// 	"qr.png",
+	// )
+	// if err != nil {
+	// 	fmt.Println("write error")
+	// }
+
+	err := qrcode.WriteColorFile(
+		"https://tofu-rosa.herokuapp.com/graphql",
+		qrcode.Medium,
+		256,
+		color.White,
+		color.RGBA{10, 100, 250, 255},
+		"qr.png",
+	)
+	if err != nil {
+		fmt.Println("write error")
+	}
+
+	fi, err := os.Open("qr.png")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer fi.Close()
+	qrmatrix, err := qrdecode.Decode(fi)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(qrmatrix.Content)
 }
