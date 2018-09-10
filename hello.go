dd01bde7b2b26c9d904a4944b192a9534da6502d
Zuolar <yam8511@gmail.com>
2018-09-11 03:13:55 +0800

    加入go-server的服務

--- hello.go
+++ hello.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("Hello World")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello E.F.K")
+	})
+
+	r.GET("/api", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "Hello API")
+	})
+
+	r.Run()
 }
