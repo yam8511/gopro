f9551197d85eaaf029ab39ae9771128eaa31c271
Zuolar <yam8511@gmail.com>
2018-09-16 14:35:49 +0800

    試試Callback

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 	"syscall/js"
 )
 
+func getElementByID(id string) js.Value {
+	return js.Global().Get("document").Call("getElementById", id)
+}
+
 func add(i []js.Value) {
-	value1 := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
-	value2 := js.Global().Get("document").Call("getElementById", i[1].String()).Get("value").String()
+	value1 := getElementByID(i[0].String()).Get("value").String()
+	value2 := getElementByID(i[1].String()).Get("value").String()
 
 	int1, _ := strconv.Atoi(value1)
 	int2, _ := strconv.Atoi(value2)
@@ -15,33 +20,63 @@ func add(i []js.Value) {
 	// sum := i[0].Int() + i[1].Int()
 	// js.Global().Set("output", js.ValueOf(sum))
 	// println(js.ValueOf(sum).String())
-	js.Global().Get("document").Call("getElementById", i[2].String()).Set("value", int1+int2)
+	output := getElementByID(i[2].String())
+	if output != js.Null() {
+		output.Set("innerText", int1+int2)
+	}
 	js.Global().Set("output", int1+int2)
 	println(int1 + int2)
 }
 
 func substract(i []js.Value) {
-	value1 := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
-	value2 := js.Global().Get("document").Call("getElementById", i[1].String()).Get("value").String()
-
-	int1, _ := strconv.Atoi(value1)
-	int2, _ := strconv.Atoi(value2)
-
-	js.Global().Get("document").Call("getElementById", i[2].String()).Set("value", int1-int2)
+	int1 := i[0].Int()
+	int2 := i[1].Int()
+	i[2].Invoke(int1 - int2)
 	js.Global().Set("output", int1-int2)
 	println(int1 - int2)
 }
 
+// 方法中心
+func goCall(args []js.Value) {
+	if len(args) == 0 {
+		fmt.Println("缺少參數")
+		return
+	}
+	if args[0].Type() != js.TypeString {
+		fmt.Println("方法名稱錯誤")
+		return
+	}
+	method := args[0].String()
+	fmt.Println("OK", method)
+	if len(args) > 1 {
+	}
+	args = args[1:]
+	fmt.Println("參數", args, len(args))
+	cb, ok := methods[method]
+	if !ok {
+		fmt.Println("方法名稱錯誤")
+		return
+	}
+
+	cb(args)
+}
+
+var methods = map[string]func([]js.Value){
+	"Add":       add,
+	"Substract": substract,
+}
+
+// 註冊給JS頁面可使用的方法名稱
 func registerCallbacks() {
-	js.Global().Set("Add", js.NewCallback(add))
-	js.Global().Set("Substract", js.NewCallback(substract))
+	for method, cb := range methods {
+		js.Global().Set(method, js.NewCallback(cb))
+	}
+	js.Global().Set("GoCall", js.NewCallback(goCall))
 }
 
 func main() {
-	c := make(chan int)
-
-	println("Go WebAssembly Initialized.")
 	registerCallbacks()
-
+	println("Go WebAssembly Initialized.")
+	c := make(chan int)
 	<-c
 }
