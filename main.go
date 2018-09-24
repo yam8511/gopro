5be3d8113a2e9fc70d2b1456ff105a1873fa3bff
Zuolar <yam8511@gmail.com>
2018-09-24 22:57:46 +0800

    regular express

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"regexp"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	reg, err := regexp.Compile("/posts/[0-9]+")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	ok := reg.MatchString("/posts/123")
+	fmt.Println("Hello World ---> ", ok)
 }
