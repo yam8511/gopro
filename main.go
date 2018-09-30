c3c8708bce9b506c0789c976fa2ed1d418913c24
Zuolar <yam8511@gmail.com>
2018-10-01 01:52:10 +0800

    測試gpio

--- main.go
+++ main.go
@@ -1,9 +1,47 @@
+/*
+
+A PWM example by @Drahoslav7, using the go-rpio library
+
+Toggles a LED on physical pin 19 (mcu pin 10)
+Connect a LED with resistor from pin 19 to ground.
+
+*/
+
 package main
 
 import (
-	"fmt"
+	"os"
+	"time"
+
+	"github.com/stianeikeland/go-rpio"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	err := rpio.Open()
+	if err != nil {
+		os.Exit(1)
+	}
+	defer rpio.Close()
+
+	power := rpio.Pin(18)
+	power.High()
+
+	pin := rpio.Pin(16)
+	pin.Mode(rpio.Pwm)
+	pin.Freq(64000)
+	pin.DutyCycle(0, 32)
+	// the LED will be blinking at 2000Hz
+	// (source frequency divided by cycle length => 64000/32 = 2000)
+
+	// five times smoothly fade in and out
+	for i := 0; i < 5; i++ {
+		for i := uint32(0); i < 32; i++ { // increasing brightness
+			pin.DutyCycle(i, 32)
+			time.Sleep(time.Second / 32)
+		}
+		for i := uint32(32); i > 0; i-- { // decreasing brightness
+			pin.DutyCycle(i, 32)
+			time.Sleep(time.Second / 32)
+		}
+	}
 }
