8bcd4b7453d754bcb161226f810d78135e8342a5
Zuolar <yam8511@gmail.com>
2018-10-01 00:32:28 +0800

    可以同時ping批量的ip

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"time"
 )
 
 func main() {
+	for i := 0; i < 255; i++ {
+		go func(i int) {
+			ip := "10.42.0." + strconv.Itoa(i)
+			cmd := exec.Command("ping", ip)
+			cmd.Stderr = os.Stderr
+			cmd.Stdout = os.Stdout
+			cmd.Start()
+			time.Sleep(time.Second * 5)
+			cmd.Process.Kill()
+		}(i)
+	}
+	time.Sleep(time.Second * 6)
 	fmt.Println("Hello World")
 }
