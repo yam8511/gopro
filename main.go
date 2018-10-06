1ddea8cd03dd0ddbc0b9a5edf5d78496c45b9b90
Zuolar <yam8511@gmail.com>
2018-10-06 21:38:18 +0800

    驗證信箱、手機、密碼

--- main.go
+++ main.go
@@ -6,13 +6,48 @@ import (
 	"regexp"
 )
 
+// 驗證用的正則表達
+const (
+	RegexpEmail    = "^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$"
+	RegexpPassword = "^[\\w\\d]{6,14}$"
+	RegexpPhone    = "^[0-9]{10}$"
+)
+
 func main() {
-	reg, err := regexp.Compile("/posts/[0-9]+")
+	invalidEmail := "yam8511gmail.com"
+	email := "yam8511@gmail.com"
+	invalidPassword := "demo12"
+	password := "demo123"
+	invalidPhone := "0912-345-678"
+	phone := "0912345678"
+
+	reg, err := regexp.Compile(RegexpEmail)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
+	ok := reg.MatchString(email)
+	fmt.Printf("Email: %s ---> %v\n", email, ok)
+	ok = reg.MatchString(invalidEmail)
+	fmt.Printf("Invalid Email: %s ---> %v\n", invalidEmail, ok)
 
-	ok := reg.MatchString("/posts/123")
-	fmt.Println("Hello World ---> ", ok)
+	reg, err = regexp.Compile(RegexpPassword)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	ok = reg.MatchString(password)
+	fmt.Printf("Password: %s ---> %v\n", password, ok)
+	ok = reg.MatchString(invalidPassword)
+	fmt.Printf("Invalid Password: %s ---> %v\n", invalidPassword, ok)
+
+	reg, err = regexp.Compile(RegexpPhone)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	ok = reg.MatchString(phone)
+	fmt.Printf("Phone: %s ---> %v\n", phone, ok)
+	ok = reg.MatchString(invalidPhone)
+	fmt.Printf("Invalid Phone: %s ---> %v\n", invalidPhone, ok)
 }
