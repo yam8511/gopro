901ee9170972e3ef7745ffc5a7ea6140fdba8757
Zuolar <yam8511@gmail.com>
2018-10-10 20:16:26 +0800

    測試panic在不同的goroutine的情況

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
+	defer func() {
+		if except := recover(); except != nil {
+			fmt.Println("主程序接收到panic --->", except)
+		}
+	}()
 	fmt.Println("Hello World")
+	go func() {
+		defer func() {
+			if except := recover(); except != nil {
+				fmt.Println("副程序接收到panic --->", except)
+			}
+		}()
+		h()
+	}()
+	// go h() // recover 無法接收不同 goroutine 的 panic
+	time.Sleep(time.Second)
+}
+
+func h() {
+	panic("Shutdown")
 }
