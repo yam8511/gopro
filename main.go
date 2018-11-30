575192cb9825caeefbb80a26d6725c2985240bbf
Zoular <yam8511_li@mail.chungyo.net>
2018-11-30 14:14:44 +0800

    [坑]字串相同，但是判斷不一樣，原因為bytes不同

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Hello World")
+	s, _ := base64.URLEncoding.DecodeString("SABlAGwAbABvAA==")
+	fmt.Println(s, string(s), string(s) == "Hello")
+	s = bytes.Replace(s, []byte{0}, nil, -1)
+	fmt.Println(s, string(s), string(s) == "Hello")
 }
