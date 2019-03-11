0003aff61adb4ec5f7c9d55fc299623e92f057e6
Zoular <yam8511_li@mail.chungyo.net>
2019-03-11 14:19:08 +0800

    [Add]時戳小工具

--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	if len(os.Args) < 2 {
+		fmt.Println("請輸入時戳, 例如: ts 1552036736 1552037432")
+		return
+	}
+
+	for i := range os.Args {
+		if i == 0 {
+			continue
+		}
+		tss := os.Args[i]
+		ts, err := strconv.ParseInt(tss, 10, 64)
+		if err != nil {
+			fmt.Printf("%d) %s : NaN\n", i, tss)
+			continue
+		}
+
+		t := time.Unix(ts, 0)
+		fmt.Printf("%d) %s : %s\n", i, tss, t.Format(time.RFC3339))
+	}
 }
