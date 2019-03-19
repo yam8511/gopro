9c35769b91428093fd6c2ac04850509ced216cbb
Zuolar <yam8511@gmail.com>
2019-03-20 01:41:08 +0800

    [Change] go1.11 -> go1.12, js.NewCallback -> js.FuncOf

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func getElementByID(id string) js.Value {
 	return js.Global().Get("document").Call("getElementById", id)
 }
 
-func add(i []js.Value) {
+func add(this js.Value, i []js.Value) interface{} {
 	value1 := getElementByID(i[0].String()).Get("value").String()
 	value2 := getElementByID(i[1].String()).Get("value").String()
 
@@ -26,42 +26,45 @@ func add(i []js.Value) {
 	}
 	js.Global().Set("output", int1+int2)
 	println(int1 + int2)
+	return int1 + int2
 }
 
-func substract(i []js.Value) {
+func substract(this js.Value, i []js.Value) interface{} {
 	int1 := i[0].Int()
 	int2 := i[1].Int()
 	i[2].Invoke(int1 - int2)
 	js.Global().Set("output", int1-int2)
 	println(int1 - int2)
+	return int1 - int2
 }
 
 // 方法中心
-func goCall(args []js.Value) {
+func goCall(this js.Value, args []js.Value) interface{} {
 	if len(args) == 0 {
 		fmt.Println("缺少參數")
-		return
+		return 0
 	}
 	if args[0].Type() != js.TypeString {
 		fmt.Println("方法名稱錯誤")
-		return
+		return 0
 	}
 	method := args[0].String()
 	fmt.Println("OK", method)
 	if len(args) > 1 {
 	}
 	args = args[1:]
-	fmt.Println("參數", args, len(args))
+	fmt.Println("參數 this", this)
+	fmt.Println("參數 args", args, len(args))
 	cb, ok := methods[method]
 	if !ok {
 		fmt.Println("方法名稱錯誤")
-		return
+		return 0
 	}
 
-	cb(args)
+	return cb(this, args)
 }
 
-var methods = map[string]func([]js.Value){
+var methods = map[string]func(js.Value, []js.Value) interface{}{
 	"Add":       add,
 	"Substract": substract,
 }
@@ -69,9 +72,9 @@ var methods = map[string]func([]js.Value){
 // 註冊給JS頁面可使用的方法名稱
 func registerCallbacks() {
 	for method, cb := range methods {
-		js.Global().Set(method, js.NewCallback(cb))
+		js.Global().Set(method, js.FuncOf(cb))
 	}
-	js.Global().Set("GoCall", js.NewCallback(goCall))
+	js.Global().Set("GoCall", js.FuncOf(goCall))
 }
 
 func main() {
