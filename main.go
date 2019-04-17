0bba21337bf009b47405786497506e5e97ed9565
Zoular <yam8511_li@mail.chungyo.net>
2019-04-17 12:00:44 +0800

    PProf

--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
-	"fmt"
+	"net/http/pprof"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	r := gin.New()
+	r.GET("/", index)
+	p := r.Group("/debug/pprof")
+	p.GET("/", pprofIndex)
+	p.GET("/:name", pprofIndex)
+	r.Run(":8000")
+}
+
+func pprofIndex(c *gin.Context) {
+	switch c.Param("name") {
+	case "profile":
+		pprof.Profile(c.Writer, c.Request)
+	default:
+		pprof.Index(c.Writer, c.Request)
+	}
+}
+
+func index(c *gin.Context) {
+	c.JSON(200, getData())
+}
+
+func getData() []string {
+	obj := []string{}
+	for i := 0; i < 10000; i++ {
+		obj = append(obj, strconv.Itoa(i))
+	}
+	return obj
 }
