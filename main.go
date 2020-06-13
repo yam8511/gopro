35db406f640722679e1d48129939772a69fc1cf2
Zuolar <yam8511@gmail.com>
2020-06-13 13:20:24 +0800

    啟動server, 自動顯示當下資料夾的檔案, 並且自動打開瀏覽器

--- main.go
+++ main.go
@@ -1,9 +1,73 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"os/signal"
+	"runtime"
+	"strings"
+	"syscall"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	port := flag.Int("p", 2020, "監聽port")
+	flag.Parse()
+
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.StaticFS("/", http.Dir("."))
+
+	var l net.Listener
+	var err error
+	for {
+		l, err = net.Listen("tcp", fmt.Sprintf(":%d", *port))
+		if err != nil {
+			if strings.Contains(err.Error(), "address already in use") {
+				*port++
+				continue
+			}
+
+			log.Fatal(err)
+		}
+		break
+	}
+
+	server := http.Server{Handler: r}
+	go server.Serve(l)
+
+	runtime.Gosched()
+	host := fmt.Sprintf("http://127.0.0.1:%d", *port)
+	fmt.Println(host)
+	openbrowser(host)
+	sig := make(chan os.Signal)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	<-sig
+	server.Shutdown(context.Background())
+}
+
+func openbrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		fmt.Println("Open Browser Error: ", err)
+	}
 }
