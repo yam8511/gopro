7b807d23876380bbb30855760f8f7c5d5245baf9
Zoular <yam8511_li@mail.chungyo.net>
2020-09-30 11:29:46 +0800

    使用goquery尋找html元素

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module gopro
 
 go 1.13
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.1
+	github.com/andybalholm/cascadia v1.2.0 // indirect
+	github.com/sirupsen/logrus v1.7.0
+	golang.org/x/net v0.0.0-20200927032502-5d4f70055728 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,67 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
+func ExampleScrape(page string) {
+	hostname := "https://studygolang.com"
+	// Request the HTML page.
+	res, err := http.Get(hostname + "/go/weekly?p=" + page)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res.Body.Close()
+
+	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+		if l, ok := s.Attr("href"); ok &&
+			strings.HasPrefix(l, "/topics/") &&
+			!strings.Contains(l, "#") &&
+			!strings.HasPrefix(l, "/topics/new") {
+
+			// Request the HTML page.
+			res, err := http.Get(hostname + l)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if res.StatusCode != 200 {
+				log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+			}
+
+			// Load the HTML document
+			topic, err := goquery.NewDocumentFromReader(res.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			res.Body.Close()
+
+			findWhat := false
+			topic.Find("div.content.markdown-body").Each(func(j int, content *goquery.Selection) {
+				findWhat = strings.Contains(content.Text(), "what")
+			})
+			if findWhat {
+				fmt.Println("找到what!!! ", s.Text(), l)
+			} else {
+				fmt.Println(s.Text(), l)
+			}
+		}
+	})
+}
+
 func main() {
-	fmt.Println("Hello World")
+	ExampleScrape("1")
+	ExampleScrape("2")
 }
